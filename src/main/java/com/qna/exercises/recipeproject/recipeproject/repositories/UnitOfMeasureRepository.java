2b0ead1d3eb76decf74f135e21def87be4681db2
Anna Kuna <ana.kuna@gmail.com>
2019-03-03 12:51:19 +0100

    added bootstrap on startup, added RecipeService, updated Controller, updated index.html

--- src/main/java/com/qna/exercises/recipeproject/recipeproject/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/qna/exercises/recipeproject/recipeproject/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.qna.exercises.recipeproject.recipeproject.repositories;
 import com.qna.exercises.recipeproject.recipeproject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
---
+++ src/main/java/com/qna/exercises/recipeproject/recipeproject/services/RecipeService.java
@@ -0,0 +1,11 @@
+package com.qna.exercises.recipeproject.recipeproject.services;
+
+import com.qna.exercises.recipeproject.recipeproject.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
+
